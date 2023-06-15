7d6f61f96ab45b6399c97240d886f98bec3b205b
Nikhil Nishad <nikhilnishad279@gmail.com>
2023-06-15 01:34:01 +0100

    integrate multilogin puppeteer
    Signed-off-by: Nikhil Nishad <nikhilnishad279@gmail.com>

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import readline from 'readline'
 import logger from './util/custom-logger.js'
 import apiClient from './util/api-client.js'
+import http from 'http'
 import PuppeteerInstagram from './lib/puppeteerInstagram.js'
 
 function prompt (question) {
@@ -14,6 +15,48 @@ function prompt (question) {
       rl.close()
       resolve(answer)
     })
+  })
+}
+
+async function startProfile (profiles) {
+  // Replace profileId value with existing browser profile ID.
+  const profileId = 'xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx'
+  const mlaPort = 35000
+
+  /* Send GET request to start the browser profile by profileId.
+  Returns web socket as response which should be passed to puppeteer.connect */
+  http.get(`http://127.0.0.1:${mlaPort}/api/v1/profile/start?automation=true&puppeteer=true&profileId=${profileId}`, (resp) => {
+    let data = ''
+
+    // Receive response data by chunks
+    resp.on('data', (chunk) => {
+      data += chunk
+    })
+
+    /* The whole response data has been received. Handling JSON Parse errors,
+    verifying if ws is an object and contains the 'value' parameter. */
+    resp.on('end', () => {
+      let ws
+      try {
+        ws = JSON.parse(data)
+      } catch (err) {
+        console.log(err)
+      }
+      // eslint-disable-next-line no-prototype-builtins
+      if (typeof ws === 'object' && ws.hasOwnProperty('value')) {
+        console.log(`Browser websocket endpoint: ${ws.value}`)
+        profiles.forEach(profile => {
+          profile.remainingAccounts.forEach(() => {
+            new PuppeteerInstagram(ws.value, profile?.network?.proxy)
+              .signup(profile.uuid)
+              .then(r => { console.log('Program Executed') })
+              .catch(e => console.error(e))
+          })
+        })
+      }
+    })
+  }).on('error', (err) => {
+    console.log(err.message)
   })
 }
 
@@ -50,14 +93,7 @@ async function main () {
       const response = await apiClient.get('http://localhost:3001/profile/unused')
       // Process the response data
       logger.info('Data:', response)
-      response.profiles.forEach(profile => {
-        profile.remainingAccounts.forEach(() => {
-          new PuppeteerInstagram({ headless: false })
-            .signup(profile.uuid)
-            .then(r => { console.log('Program Executed') })
-            .catch(e => console.error(e))
-        })
-      })
+      await startProfile(response.profiles)
     } catch (error) {
       logger.error('Account creation failed' + error)
     }
